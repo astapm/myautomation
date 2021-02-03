b5e0de8610a2c4aa21bb1d74f641ebcaef0b42b7
astapm <astapm@gmail.com>
2021-02-03 23:23:52 +0300

    edit viewtone.py

--- tone2csv/viewtone.py
+++ tone2csv/viewtone.py
@@ -14,16 +14,16 @@
 import csv
 import os
 
-with open('mytonecsv.csv') as f:
+with open('mytone.csv') as f:
     num_line = (sum(1 for _ in f))
 
 if num_line < 10:
-    with open('mytonecsv.csv', newline='') as File:  
+    with open('mytone.csv', newline='') as File:  
         reader = csv.reader(File)
         for row in reader:
             print(row[0],row[1],row[2],row[3],row[4],row[5])
 else:
-    r = os.system("tail -n 10 mytonecsv.csv")
+    r = os.system("tail -n 10 mytone.csv")
     print(r)
 
 print("Programm terminated normally")
