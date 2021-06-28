ab8a2b5fe9361e99b3a1256f060bb8a2aefff651
Mikhail Astapchyk <74508872+astapm@users.noreply.github.com>
2021-06-29 00:02:42 +0300

    Create csv2step.sh

---
+++ myscripts/csv2step.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Скрипт переводит csv-файл в таблицу-лесенку
+
+nameFileCSV="mycsv.csv"
+prefix=" "
+CSVseparator=";"
+
+IFS=$'\n'
+for row in $(cat $nameFileCSV)
+do
+count=0
+IFS=$CSVseparator
+  for var in ${row[*]}
+  do
+    for((i=0;i<$count;i++))
+    do                         
+    echo -n $prefix
+    done
+  count=$(($count + 1))
+  echo $var
+  done
+done
