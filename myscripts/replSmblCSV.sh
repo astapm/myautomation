93586e52288a99e409b199eb464c109febac5a87
Mikhail Astapchyk <74508872+astapm@users.noreply.github.com>
2021-07-04 23:17:31 +0300

    Create replSmblCSV.sh

---
+++ myscripts/replSmblCSV.sh
@@ -0,0 +1,38 @@
+#/bin/bash
+
+#
+# Функция проверки и замены служебных символов для поля формата CSV
+#
+# Меняем разделитель полей CSV (обычно "," или ";") на другой символ
+# еняем символ `"` на `'`, если есть
+# Меняем символ "\n" на "\u000A"
+# Требует три позиционных параметра:
+# $1 - строка поля CSV-файла
+# $2 - разделитель полей CSV-файла
+# $3 - замена для разделителя полей CSV-файла
+# GPLv3 2021 Астапчик Михаил
+
+replaceSymbolsCSV(){
+  
+  temp=${1//$2/$3}            # меняем разделитель CSV
+  temp=${temp//\"/\'}         # меняем двойную кавычку 
+  temp=${temp//"\n"/"\u000A"} # меняем перевод строки
+  echo ${temp}
+}
+#------------
+
+#Пример использования
+separatorCSV=";"              # разделитель CSV
+sep_replace="\u003B"          # замена для разделителя CSV
+
+CSVfield="vcb;vn\n vn\"hj8"   # пример строки поля CSV
+
+# Сначало выводим первоначальную строку поля CSV
+echo "Поле CSV до замены, echo и printf"
+echo $CSVfield
+printf "$CSVfield\n"
+
+# Выводим строку поля CSV после replaceSymbolsCSV
+echo "Поле CSV после замены, echo и printf"
+echo $(replaceSymbolsCSV "$CSVfield" $separatorCSV $sep_replace)
+printf "$(replaceSymbolsCSV "$CSVfield" $separatorCSV $sep_replace)\n"
